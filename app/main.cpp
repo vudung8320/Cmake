069ba19d36cb13d3c5f78a3bc5e5aacd95799d86
vudung8320 <vudung8320@gmail.com>
2024-03-10 00:39:40 +0700

    Lesson 4

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
 #include "my_lib.h"
+#include "config.hpp"
 
 
 int main(void)
 {
+  std::cout << "Project name: " << project_name << std::endl;
+  std::cout << "Project version: " << project_version << std::endl;
+
   print_hello_world();
 
   return 0;
